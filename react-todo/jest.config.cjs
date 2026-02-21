21b8da339354e00bd283cbece0c79eeb9932b31e
Ntinyari1 <ntinyariyvonne823@gmail.com>
2026-02-21 17:39:51 +0300

    task 3

---
+++ react-todo/jest.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+  },
+};
